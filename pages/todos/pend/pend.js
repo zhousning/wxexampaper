1f77a2a19dbcdb15822c59fe70eea60802349284
zhousningg <244305150@qq.com>
2022-01-17 17:22:49 +0800

    添加轮播图

--- pages/todos/pend/pend.js
+++ pages/todos/pend/pend.js
@@ -10,6 +10,36 @@ Component({
     CustomBar: app.globalData.CustomBar,
     gridCol:3,
     skin: false,
+    cardCur: 0,
+    swiperList: [{
+      id: 0,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
+    }, {
+      id: 1,
+        type: 'image',
+        url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84001.jpg',
+    }, {
+      id: 2,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big39000.jpg'
+    }, {
+      id: 3,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
+    }, {
+      id: 4,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'
+    }, {
+      id: 5,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big21016.jpg'
+    }, {
+      id: 6,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big99008.jpg'
+    }],
     iconList: [{
       icon: 'cardboardfill',
       color: 'blue',
@@ -61,5 +91,22 @@ Component({
       badge: 0,
       name: '版权'
     }],
+  },
+  lifetimes: {
+    attached: function() {
+      //this.towerSwiper('swiperList'); // 初始化towerSwiper 传已有的数组名即可
+    }
+  },
+  methods: {
+    DotStyle: function(e) {
+      this.setData({
+        DotStyle: e.detail.value
+      })
+    },
+    cardSwiper: function(e) {
+      this.setData({
+        cardCur: e.detail.current
+      })
+    }
   }
 })
