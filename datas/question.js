0016e70457a2123cc35db9924e55fca042e2cf58
zhousningg <244305150@qq.com>
2022-01-17 12:08:08 +0800

    添加显示题目

---
+++ datas/question.js
@@ -0,0 +1,39 @@
+var questions = {
+  "time": "2022-01-15",
+  "items": [
+    {
+      "title": "1、安全生产法一般指中华人民共和国安全生产法。 2002年6月29日第九届全国人民代表大会常务委员会第二十八次会议通过，根据2009年8月27日第十一届全国人民代表大会常务委员会第十次会议《关于修改部分法律的决定》第一次修正，根据2014年8月31日第十？",
+      "options": [
+        {"id": 0, "value": "A", "content": "安全部门"},
+        {"id": 1, "value": "B", "content": "水务部门"},
+        {"id": 2, "value": "C", "content": "监察部门"},
+        {"id": 3, "value": "D", "content": "工程部门"}
+      ],
+      "answer": "B"
+    },
+    {
+      "title": "2、安全生产法是给哪个部门的？",
+      "options": [
+        {"id": 0, "value": "A", "content": "生产部门"},
+        {"id": 1, "value": "B", "content": "控制部门"},
+        {"id": 2, "value": "C", "content": "礼部部门"},
+        {"id": 3, "value": "D", "content": "大哥部门"}
+      ],
+      "answer": "A"
+    },
+    {
+      "title": "3、安全生产法是给哪个部门的？",
+      "options": [
+        {"id": 0, "value": "A", "content": "上书房部门"},
+        {"id": 1, "value": "B", "content": "军机处门"},
+        {"id": 2, "value": "C", "content": "步军统领门"},
+        {"id": 3, "value": "D", "content": "工程部门"}
+      ],
+      "answer": "C"
+    }
+  ]
+}
+
+module.exports = {
+  questions: questions
+}
--- pages/todos/process/process.js
+++ pages/todos/process/process.js
@@ -1,4 +1,5 @@
 // pages/todos/process/process.js
+const question_datas = require('../../../datas/question.js')
 const app = getApp()
 
 Component({
@@ -7,6 +8,60 @@ Component({
   },
   data: {
     StatusBar: app.globalData.StatusBar,
-    CustomBar: app.globalData.CustomBar
+    CustomBar: app.globalData.CustomBar,
+    questions: [],
+    current: 0,
+    previous: 0,
+    next: 0,
+    answer: '',
+    previous_disabled: true,
+    next_disabled: false
+  },
+  methods: {
+    getAnswer(e) {
+      this.setData({
+        answer: e.detail.value
+      })
+    },
+    previousQes: function () {
+      var val = this.data.current - 1
+      if (val == 0) {
+        this.setData({
+          previous_disabled: true,
+          next_disabled: false,
+          current: val
+        })
+      } else {
+        this.setData({
+          previous_disabled: false,
+          next_disabled: false,
+          current: val
+        })
+      }
+    },
+    nextQes: function () {
+      var val = this.data.current + 1
+      var qes = this.data.questions
+      if (val == qes.length-1) {
+        this.setData({
+          previous_disabled: false,
+          next_disabled: true,
+          current: val
+        })
+      } else {
+        this.setData({
+          previous_disabled: false,
+          next_disabled: false,
+          current: val
+        })
+      }
+    }
+  },
+  lifetimes: {
+    attached: function () {
+      this.setData({
+        questions: question_datas.questions.items
+      })
+    }
   }
 })
