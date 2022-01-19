4b6d16cdd052be54ce422206821b52b2271c741e
zhousningg <244305150@qq.com>
2022-01-19 10:45:53 +0800

    修改一些内容

--- pages/todos/qes_show/qes_show.js
+++ pages/todos/qes_show/qes_show.js
@@ -11,7 +11,7 @@ Page({
     current: 0,
     previous: 0,
     next: 0,
-    qes_type: [],
+    qes_type: '',
     answer: '', //用于换题后radio清空
     myAnswer: '', //用于给正确答案显示绿色字体
     previous_disabled: true,
@@ -23,7 +23,7 @@ Page({
     var answer = e.detail.value
     var myAnswer = ''
 
-    if (answer == trueAnswer) {
+    if (trueAnswer == answer) {
       myAnswer = answer
     }
     this.setData({
@@ -97,8 +97,24 @@ Page({
       },
       success: function (res) {
         var objs = res.data;
+        var qes_type = '';
+        switch (type) {
+          case 'singles':
+            qes_type = '单选题'
+            break;
+            case 'mcqs':
+            qes_type = '多选题'
+            break;
+            case 'tofs':
+            qes_type = '判断题'
+            break;
+            case 'qaas':
+            qes_type = '问答题'
+            break;
+        }
         that.setData({
-          questions: objs
+          questions: objs,
+          qes_type: qes_type
         })
         wx.hideLoading();
       },
